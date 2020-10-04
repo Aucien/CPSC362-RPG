066a7ba6398df76d016f76c2996fdd8d728f437c
Gordon Dan <blu3sag3@gmail.com>
2020-10-04 10:10:15 -0700

    Organized Files, added in gravity and jump

--- CharacterSourceCode.gd
+++ CharacterSourceCode.gd
@@ -1,40 +1,41 @@
 extends KinematicBody2D
 
-const MAX_SPEED = 200
-const FRICTION = 800
-const ACCELERATION = 800
+export var max_speed = 200
+export var friction = 800
+export var acceleration = 800
+export var gravity = 15
 
 var velocity = Vector2.ZERO
 
 onready var initial_scale = scale
-onready var animationPlayer = $AnimationPlayer
-onready var animationState = $AnimationTree.get("parameters/playback")
-#func _ready():
-	#$AnimationTree.active = true
+onready var animation_state = $AnimationTree.get("parameters/playback")
+
 
 func _physics_process(delta):
 	keyboard_input(delta)
 	
+	velocity.y +=gravity;
+	velocity = move_and_slide(velocity, Vector2.UP)
+	
 func keyboard_input(delta):
 	var input_vector = Vector2.ZERO
 	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
-	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	if Input.is_action_just_pressed("ui_up"):
+		input_vector.y = -100.0
 	input_vector = input_vector.normalized()
-	
+	print(input_vector)
 	if input_vector != Vector2.ZERO:
-		#$AnimationTree.set("parameters/Idle", input_vector)
-		#$AnimationTree.set("parameters/Run", input_vector)
 		if input_vector.x > 0:
+			animation_state.travel("Run")
 			scale.x = initial_scale.x * sign(scale.y)
 		elif input_vector.x < 0:
+			animation_state.travel("Run")
 			scale.x = -initial_scale.x * sign(scale.y)
-		animationState.travel("Run")
-		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
+		if velocity.y < 0:
+			animation_state.travel("Jump")
+		#velocity = velocity.move_toward(input_vector * max_speed, acceleration * delta)
+		
+		velocity = input_vector * max_speed
 	else:
-		animationState.travel("Idle")
-		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
-	
-	#var movement = input_vector * MAX_SPEED * delta
-	#input_vector = move_and_slide(movement)
-	velocity = move_and_slide(velocity)
-	
+		animation_state.travel("Idle")
+		velocity = velocity.move_toward(Vector2.ZERO, friction * delta)
