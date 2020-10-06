a9d438273f0801fb13648cbe6952bba6e859c157
dngo94 <69218823+dngo94@users.noreply.github.com>
2020-10-06 12:55:49 -0700

    Update 10/6/2020
    -Fixed "spaghetti" code:
    
    +Var  Acceleration is now being used
    +Added Min(), max(), and lerp()
    
    -Sorted assests, added new subfolders.

--- src/Scenes/Character/CharacterSourceCode.gd
+++ src/Scenes/Character/CharacterSourceCode.gd
@@ -1,15 +1,12 @@
 extends KinematicBody2D
 
 const MAX_SPEED = 200
-const GRAVITY = 15
-const FRICTION = 800
-const ACCELERATION = 800
-const JUMP_PW= -350
+const GRAVITY = 20
+const ACCELERATION = 80
+const JUMP_PW= -400
 
-onready var initial_scale = scale
 var velocity = Vector2.ZERO
-var on_ground = false
-
+onready var initial_scale = scale
 onready var animationPlayer = $AnimationPlayer
 onready var animationState = $AnimationTree.get("parameters/playback")
 
@@ -21,35 +18,37 @@ func _physics_process(delta):
 
 func move_state(delta):
 	var input_vector = Vector2.ZERO
+	var on_ground = false
+	
 	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	input_vector = input_vector.normalized()
 	
 	if input_vector != Vector2.ZERO:
 		if input_vector.x > 0:
-			velocity.x = MAX_SPEED
+			velocity.x = min(velocity.x + ACCELERATION, MAX_SPEED)
 			scale.x = initial_scale.x * sign(scale.y)
 		elif input_vector.x < 0:
-			velocity.x = -MAX_SPEED
+			velocity.x = max(velocity.x - ACCELERATION, -MAX_SPEED)
 			scale.x = -initial_scale.x * sign(scale.y)
 		animationState.travel("Run")
 	else:
-		velocity.x = 0
 		animationState.travel("Idle")
-	
-	if Input.is_action_pressed("ui_accept") and is_on_floor():
-		animationState.travel("Jump")
-		velocity.y = JUMP_PW
+		on_ground = true
 	
 	velocity.y += GRAVITY 
 	
 	if is_on_floor():
-		on_ground == true
+		if Input.is_action_pressed("ui_accept"):
+			velocity.y = JUMP_PW
+		if on_ground == true:
+			velocity.x = lerp(velocity.x, 0 , 0.2)
 	else:
-		on_ground == false	
 		if velocity.y < 0:
 			animationState.travel("Jump")
 		else:
 			animationState.travel("Fall")
+		if on_ground == true:
+			velocity.x = lerp(velocity.x, 0 , 0.05)
 	
 	velocity = move_and_slide(velocity, Vector2.UP)
 	
