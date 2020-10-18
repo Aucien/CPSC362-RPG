f2f1e5fce55ed696209bbc67c18472ac1edde44b
Billy <engbilly350@yahoo.com>
2020-10-18 02:38:36 -0700

    I failed in making my monster fly in that time frame I have had, It needs more work I know but I have done my best so far. My enemy can take hits and presents a dead animation and It can detect objects and move to the left and right however, when it dies, it does not stop moving. The code line for that should be  velocity = Vector2(0,0) but it is still not working .. probably because it is not in the right code line or there might be another way

---
+++ src/Scenes/Enemies/FlyingEye.gd
@@ -0,0 +1,72 @@
+extends KinematicBody2D
+
+const GRAVITY = 10
+const SPEED = 30
+const FLOOR = Vector2(0,-1)
+
+var velocity = Vector2()
+
+var direction = 1
+
+var state = IDLE
+
+enum {
+	IDLE,
+	TAKE_HIT,
+	DEATH
+}
+var hp = 1
+
+func _ready():
+	pass
+	
+func flying(delta):
+	$Sprite.play("Flying")
+
+func take_hit(delta):
+	$Sprite.play("Damage")
+	
+func dead(delta):
+	velocity = Vector2(0,0)
+	$Sprite.play("Death")
+
+	
+func _physics_process(delta):
+	
+		velocity.x = SPEED * direction
+		
+		if direction == 1:
+			$Sprite.flip_h = false
+		else:
+			$Sprite.flip_h = true
+			
+		velocity.y += GRAVITY
+		
+		velocity = move_and_slide(velocity, FLOOR)
+	
+	
+		if is_on_wall():
+			direction = direction * -1
+		#$RayCast2D.position.x *= -1
+
+	#if $RayCast2D.is_colliding() == false:
+		#direction = direction* -1
+		#$RayCast2D.position.x *= -1
+		match state:
+			IDLE:
+				flying(delta)
+			TAKE_HIT:
+				take_hit(delta)
+			DEATH:
+				dead(delta)
+
+		if hp <= 0:
+			state = DEATH
+			velocity = Vector2(0,0)
+func _on_Hurtbox_area_entered(area):
+	if area.name == "PlayerHitbox":
+		hp -= area.damage
+		state = TAKE_HIT
+		print(hp)
+		
+		
