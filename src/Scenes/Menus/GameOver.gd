b3e0087ee18a1b9562ce33c488ab2e8f656a6da8
Gordon Dan <blu3sag3@gmail.com>
2020-11-22 10:10:05 -0800

    Added extra scenes for winning

--- src/Scenes/Menus/GameOver.gd
+++ src/Scenes/Menus/GameOver.gd
@@ -3,7 +3,7 @@ extends Control
 #onready var animationPlayer = $AnimationPlayer
 
 func _ready():
-	for button in $Menu/CenterRow/Buttons.get_children():
+	for button in $Menu.get_children():
 		button.connect("pressed", self, "on_ButtonPressed", [button.scene_to_load])
 
 func on_ButtonPressed(scene_to_load):
